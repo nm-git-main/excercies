4b1a929dbda7e0a2d5d8d89fa9f3cbb2a70faf2b
KapiCytryna <kacperczura9@gmail.com>
2020-05-23 13:30:18 +0200

    Wydzielenie czyszczenia i konvertowania stringa do tablicy double do oddzielnej metody

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -6,23 +6,12 @@ public class App {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Proszę podać kolejno trzy liczby całkowite, oddzielone przecinkiem np. 1,2,3");
-        String[] splittedLine = scanner.nextLine()
-                .trim()
-                .replaceAll(" ","")
-                .split(",");
-
-        double[] abc = new double[splittedLine.length];
-
-        for(int i = 0; i < splittedLine.length; i++){
-            try {
-                abc[i] = Double.parseDouble(splittedLine[i]);
-            } catch(NumberFormatException e) {
-                System.out.print("Zły format danych.");
-                return;
-            }
-        }
+        String line = scanner.nextLine();
+        double[] abc = new double[3];
 
         try {
+            abc = cleanAndConvertToDoubleArray(line);
+
             RootsOfSquareEquation pierwiastki = RootsCalculator.calculateRootsOfSquareEquation(abc[0], abc[1], abc[2]);
             if(pierwiastki.getX1()==pierwiastki.getX2()){
                 System.out.println("Jeden pierwiastek x = " + pierwiastki.getX1());
@@ -34,6 +23,21 @@ public class App {
             System.out.println("Parametry a: " + abc[0] + ", b: " + abc[1] + ", c: " + abc[2] + " spowodowały ujemną deltę.");
         } catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Za mało argumentów.");
+        } catch(NumberFormatException e) {
+            System.out.println("Zły format danych.");
         }
+    }
+
+    public static double[] cleanAndConvertToDoubleArray(String line) {
+        String[] splittedLine = line.trim()
+                .replaceAll(" ","")
+                .split(",");
+
+        double[] abc = new double[splittedLine.length];
+
+        for(int i = 0; i < splittedLine.length; i++) {
+            abc[i] = Double.parseDouble(splittedLine[i]);
+        }
+        return abc;
     }
 }
